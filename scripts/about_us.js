89abe94d9555c41955330ded6db3cb94c7d731c1
DannyOppenheimer <oppenheimerd1@gmail.com>
2020-04-09 16:52:23 -0400

    Got rid of the mess of divs and made a for-loop in about_us.html

---
+++ scripts/about_us.js
@@ -0,0 +1,16 @@
+let memberData = JSON.parse('scripts/members.json');
+console.log(memberData);
+
+let cardDiv = document.createElement('DIV');
+var img = document.createElement('IMG');
+img.src = 'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+cardDiv.appendChild(document.createElement());
+
+/*
+<div>
+	<img src="/assets/images/hmm.png" />
+	<p class="label">example</p>
+	<h5>teehee</h5>
+	<p>placeholder woop woop</p>
+</div>
+*/
