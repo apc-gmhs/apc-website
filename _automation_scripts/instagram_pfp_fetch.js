2cc67ac2fad6d821b885243045b3b2eade54e87c
DannyOppenheimer <oppenheimerd1@gmail.com>
2020-04-10 19:23:02 -0400

    Stopped fs from overwriting every for loop

---
+++ _automation_scripts/instagram_pfp_fetch.js
@@ -0,0 +1,10 @@
+
+let usernames = ['jonathanoppenheimer']
+
+fetch(')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
--- _automation_scripts/profile_picture_links.js
+++ _automation_scripts/profile_picture_links.js
@@ -1,24 +1,32 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 const fs = require('fs');
 
 /* use google api to get usernames from spreadsheet and fill array */
-let usernames = [''];
+let usernames = ['jonathanoppenheimer'];
 let urls = {
-    instagram_urls: [] 
+	instagram_urls: [],
 };
 
-for (let i of usernames) {
-    fetch('https://www.instadp.com/fullsize/' + i)
-        .then(res => res.text())
-        .then(body => {
-            if(body.includes("<img class=\"picture\" src=\""))
-            {
-                let temp_pos = body.split('<img class=\"picture\" src=\"').pop();
-                let pic_url = temp_pos.substring(0, temp_pos.indexOf("\""));
+async function find() {
+	for (let i of usernames) {
+		await fetch('https://www.instadp.com/fullsize/' + i)
+			.then((res) => res.text())
+			.then((body) => {
+				if (body.includes('<img class="picture" src="')) {
+					// remove everything after the html tag for the pfp
+					let temp_pos = body.split('<img class="picture" src="').pop();
+					// only keep everything before the closing quote
+					let pic_url = temp_pos.substring(0, temp_pos.indexOf('"'));
 
-                urls.instagram_urls.push({[i]: pic_url}); 
-                var json = JSON.stringify(urls)
-                fs.writeFileSync("_data/" + "instagram_urls.json", json, 'utf8', null, 4); 
-            }
-        })
+					urls.instagram_urls.push({
+						[i]: pic_url,
+					});
+				}
+			});
+	}
 }
+
+find().then(() => {
+	// the node run command must be run in this directory for the path to work
+	fs.writeFileSync('../_data/instagram_urls.json', JSON.stringify(urls, null, 4));
+});
