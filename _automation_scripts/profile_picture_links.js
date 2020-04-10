697e611194aece1185b2c8245cb66081cf6194f9
TheBlueness <147infiniti@gmail.com>
2020-04-10 18:41:25 -0400

    create js file to grab links to instagram urls for all our members

---
+++ _automation_scripts/profile_picture_links.js
@@ -0,0 +1,24 @@
+const fetch = require("node-fetch");
+const fs = require('fs');
+
+/* use google api to get usernames from spreadsheet and fill array */
+let usernames = ['jonathanoppenheimer','jonathanoppenheimer','jonathanoppenheimer'];
+let urls = {
+    instagram_urls: [] 
+};
+
+for (let i of usernames) {
+    fetch('https://www.instadp.com/fullsize/' + i)
+        .then(res => res.text())
+        .then(body => {
+            if(body.includes("<img class=\"picture\" src=\""))
+            {
+                let temp_pos = body.split('<img class=\"picture\" src=\"').pop();
+                let pic_url = temp_pos.substring(0, temp_pos.indexOf("\""));
+
+                urls.instagram_urls.push({[i]: pic_url}); 
+                var json = JSON.stringify(urls)
+                fs.writeFileSync("instagram_urls.json", json, 'utf8', null, 4); 
+            }
+        })
+}
