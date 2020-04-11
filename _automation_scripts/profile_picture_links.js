a9397c70bf14fdb60dca04bac06f38c1e731f80d
DannyOppenheimer <oppenheimerd1@gmail.com>
2020-04-10 20:58:11 -0400

    Used google-spreadsheet API to create a formatted member list

--- _automation_scripts/profile_picture_links.js
+++ _automation_scripts/profile_picture_links.js
@@ -1,32 +1,50 @@
+'use strict';
+
 const fetch = require('node-fetch');
 const fs = require('fs');
+const GoogleSpreadsheet = require('google-spreadsheet');
+const { promisify } = require('util');
+const creds = require('./google_secret.json');
 
-/* use google api to get usernames from spreadsheet and fill array */
-let usernames = [''];
-let urls = {
-	instagram_urls: [],
+let members = {
+	members: [],
 };
 
-async function find() {
-	for (let i of usernames) {
-		await fetch('https://www.instadp.com/fullsize/' + i)
-			.then((res) => res.text())
-			.then((body) => {
-				if (body.includes('<img class="picture" src="')) {
-					// remove everything after the html tag for the pfp
-					let temp_pos = body.split('<img class="picture" src="').pop();
-					// only keep everything before the closing quote
-					let pic_url = temp_pos.substring(0, temp_pos.indexOf('"'));
+async function fill() {
+	const doc = new GoogleSpreadsheet('SPREADSHEET KEY HERE');
+	await promisify(doc.useServiceAccountAuth)(creds);
+	const info = await promisify(doc.getInfo)();
+	const sheet = info.worksheets[0];
+	let data = await promisify(sheet.getRows)({
+		offset: 1,
+	});
 
-					urls.instagram_urls.push({
-						[i]: pic_url,
-					});
-				}
-			});
+	for (let row of data) {
+		let url = await find(row.ifyestotheabovequestionwhatisyourinstagramtag);
+		members.members.push({
+			imagepath: url,
+			label: row.role,
+			name: row.whatisyourname,
+			desc: row.whatwouldyoulikeyourbiotobe35wordsorless,
+		});
 	}
 }
 
-find().then(() => {
-	// the node run command must be run in this directory for the path to work
-	fs.writeFileSync('../_data/instagram_urls.json', JSON.stringify(urls, null, 4));
+async function find(username) {
+	let pic_url;
+	await fetch('https://www.instadp.com/fullsize/' + username)
+		.then((res) => res.text())
+		.then((body) => {
+			if (body.includes('<img class="picture" src="')) {
+				// remove everything after the html tag for the pfp
+				let temp_pos = body.split('<img class="picture" src="').pop();
+				// only keep everything before the closing quote
+				pic_url = temp_pos.substring(0, temp_pos.indexOf('"'));
+			}
+		});
+	return pic_url;
+}
+
+fill().then(() => {
+	fs.writeFileSync('_data/formatted.json', JSON.stringify(members, null, 4));
 });
