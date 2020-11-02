89fb26c69a10743ccc71b4ac3ea3ba882027f0ca
BrandonWerbel <brandon@werbel.org>
2020-11-02 10:43:56 -0500

    Fix mistakes from first commit

--- scripts/compendium.js
+++ scripts/compendium.js
@@ -6,17 +6,7 @@ const nextButton = document.getElementById('next-button');
 // 60vw + 35 for margin between entries
 const ENTRY_SCROLL_FACTOR = (document.documentElement.clientWidth * 0.6) + 35;
 
-//checks to see if url specifies an entry
-if(window.location.href[window.location.href.length - 11] === '-'){
-    //goes away from set entry to first entry
-    entries[0].scrollIntoView({behavior: 'auto'});
-
-    //scrolls back to set entry
-    let currentEntry = document.getElementById('entry-' + window.location.href.substring(window.location.href.length - 10));
-    currentEntry.scrollIntoView();
-} else{
-    entries[entries.length - 2].scrollIntoView();
-}
+entries[entries.length - 2].scrollIntoView();
 
 nextButton.addEventListener('click', function() {
     entryViewer.scrollBy({ left: ENTRY_SCROLL_FACTOR})
@@ -51,5 +41,7 @@ let observer = new IntersectionObserver(function(observerEntries, _) {
     }
 }, observerOptions);
 
+//takes correct arrow buttons away from first and last entry
+observer.observe(entries[0]);
 // Subtract 2 to account for unusable last entry
 observer.observe(entries[entries.length - 2]);
